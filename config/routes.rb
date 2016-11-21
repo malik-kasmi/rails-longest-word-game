140e2b57c1b228632245a8d0ec537115b2a688b1
Malik Kasmi <malik.kasmi@gmail.com>
2016-11-21 16:47:17 +0100

    finish rails intro / 2-longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'game', to: 'word_game#game'
+  get 'score', to: 'word_game#score'
+
+  root to: 'word_game#home'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
